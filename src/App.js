9ef1b10f493d7500a00c72d01ea2d3e5c0bb9247
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2023-11-06 21:20:26 +0100

    fix: correct long and lat given with api

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ export default class App{
     getWeather(x, y){
         //url: https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=0ed3744e6c9b4f6abe8216b6abe3fe96
         //fetch, then log result
-        fetch(`https://api.weatherbit.io/v2.0/current?lat=${x}&lon=-${y}&key=0ed3744e6c9b4f6abe8216b6abe3fe96`)
+        fetch(`https://api.weatherbit.io/v2.0/current?lat=${x}&lon=${y}&key=0ed3744e6c9b4f6abe8216b6abe3fe96`)
             .then(response => response.json())
             .then(data => {
                 console.log(data);
